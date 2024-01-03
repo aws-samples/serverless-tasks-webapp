39a6378933af08a02690182df0781c35440a178e
natigold <natigold@gmail.com>
2024-01-03 12:56:15 +0200

    Updated resize script to IMDSv2

--- resize.sh
+++ resize.sh
@@ -1,11 +1,13 @@
+
 #!/bin/bash
 
 # Specify the desired volume size in GiB as a command line argument. If not specified, default to 20 GiB.
 SIZE=${1:-20}
 
 # Get the ID of the environment host Amazon EC2 instance.
-INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 60")
+INSTANCEID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id 2> /dev/null)
+REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/placement/region 2> /dev/null)
 
 # Get the ID of the Amazon EBS volume associated with the instance.
 VOLUMEID=$(aws ec2 describe-instances \
@@ -22,35 +24,39 @@ while [ \
   "$(aws ec2 describe-volumes-modifications \
     --volume-id $VOLUMEID \
     --filters Name=modification-state,Values="optimizing","completed" \
-    --query "length(VolumesModifications)" \
+    --query "length(VolumesModifications)"\
     --output text)" != "1" ]; do
-  sleep 1
+sleep 1
 done
 
-#Check if we're on an NVMe filesystem
-if [[ -e "/dev/xvda" && $(readlink -f /dev/xvda) = "/dev/xvda" ]]; then
-  # Rewrite the partition table so that the partition takes up all the space that it can.
+# Check if we're on an NVMe filesystem
+if [[ -e "/dev/xvda" && $(readlink -f /dev/xvda) = "/dev/xvda" ]]
+then
+# Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/xvda 1
-
-  # Expand the size of the file system.
-  # Check if we're on AL2
+# Expand the size of the file system.
+# Check if we're on AL2 or AL2023
   STR=$(cat /etc/os-release)
-  SUB="VERSION_ID=\"2\""
-  if [[ "$STR" == *"$SUB"* ]]; then
+  SUBAL2="VERSION_ID=\"2\""
+  SUBAL2023="VERSION_ID=\"2023\""
+  if [[ "$STR" == *"$SUBAL2"* || "$STR" == *"$SUBAL2023"* ]]
+  then
     sudo xfs_growfs -d /
   else
     sudo resize2fs /dev/xvda1
   fi
 
 else
-  # Rewrite the partition table so that the partition takes up all the space that it can.
+# Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/nvme0n1 1
 
-  # Expand the size of the file system.
-  # Check if we're on AL2
+# Expand the size of the file system.
+# Check if we're on AL2 or AL2023
   STR=$(cat /etc/os-release)
-  SUB="VERSION_ID=\"2\""
-  if [[ "$STR" == *"$SUB"* ]]; then
+  SUBAL2="VERSION_ID=\"2\""
+  SUBAL2023="VERSION_ID=\"2023\""
+  if [[ "$STR" == *"$SUBAL2"* || "$STR" == *"$SUBAL2023"* ]]
+  then
     sudo xfs_growfs -d /
   else
     sudo resize2fs /dev/nvme0n1p1
